ce735ab4ee882fb9789f3ea4d0177e33e3841599
Leandro Dias <stevenrj@hotmail.com>
2020-08-20 17:34:54 -0300

    Updates

---
+++ precache-manifest.d1886682b9b24b1949343af6af862f6e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26878b1101abb43111c46682ecb2a43",
+    "url": "/solucionaprevi/index.html"
+  },
+  {
+    "revision": "38ee3c7644bef74bf48d",
+    "url": "/solucionaprevi/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c61b294feffd3a983642",
+    "url": "/solucionaprevi/static/css/main.c123f3e1.chunk.css"
+  },
+  {
+    "revision": "38ee3c7644bef74bf48d",
+    "url": "/solucionaprevi/static/js/2.99116de0.chunk.js"
+  },
+  {
+    "revision": "e7c69aa4549a0938e155d41214f1973f",
+    "url": "/solucionaprevi/static/js/2.99116de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c61b294feffd3a983642",
+    "url": "/solucionaprevi/static/js/main.20da9440.chunk.js"
+  },
+  {
+    "revision": "ab42a83e6096dafaed36",
+    "url": "/solucionaprevi/static/js/runtime-main.bd2b1907.js"
+  },
+  {
+    "revision": "0181e1077e525ee2a91a7602bb50932e",
+    "url": "/solucionaprevi/static/media/automation.0181e107.jpg"
+  },
+  {
+    "revision": "780e8396d518ffa3ac6c05e61f440a37",
+    "url": "/solucionaprevi/static/media/logo_svg.780e8396.svg"
+  }
+]);
